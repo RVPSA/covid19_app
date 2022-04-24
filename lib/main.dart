1deceec10253e08fd153c6809badd860c04f57fc
sachinthaakesh <akeshramasinghe@gmail.com>
2022-04-25 00:54:37 +0530

    API
    1.import 'dart:convert' as convert;
    2.Update pubspecyaml file with relevent dependency by adding under  the 'cupertino_icons'
    3.import 'package:http/http.dart' as http;
    4.Handle Api
    5.Modify cards to show data which obtain from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: myHomePage(),
     );
   }
 }
-class myHomePage extends StatelessWidget {
+
+
+class myHomePage extends StatefulWidget {
   const myHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<myHomePage> createState() => _myHomePageState();
+}
+
+class _myHomePageState extends State<myHomePage> {
+
+
+  //api
+  var date,newCases,todayDeathCases,totaldeath;
+  _loadAPIData() async{
+    var response = await http.get(Uri.https('www.hpb.health.gov.lk', '/api/get-current-statistical'));
+
+    setState(() {
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+        date = jsonResponse['data']['update_date_time'];
+        newCases = jsonResponse['data']['local_new_cases'];
+        todayDeathCases = jsonResponse['data']['local_new_deaths'];
+        totaldeath = jsonResponse['data']['local_new_deaths'];
+
+      } else {
+        print('Request failed with status: ${response.statusCode}.');
+      }
+    });
+  }
+  //end of API get
+
+  @override
+  void initState(){
+    super.initState();
+    _loadAPIData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +61,7 @@ class myHomePage extends StatelessWidget {
         title: Text('Covid - 19 Daily News'),
         leading: IconButton(
           icon: Icon(Icons.campaign),
-          onPressed: (){},
+          onPressed: _loadAPIData,
         ),
       ),
       body: Center(
@@ -35,13 +71,18 @@ class myHomePage extends StatelessWidget {
               width: double.infinity,
               height: 100,
               child: Card(
-                color: Colors.amber,
+                color: Colors.amber[100],
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
                 ),
                 child: Container(
                     padding: EdgeInsets.only(left: 20.0,top: 20.0),
-                    child: Text("Date: -")
+                    child:Column(
+                      children: [
+                        Text("Date: -"),
+                        Text('${date}'),
+                      ],
+                    )
                 ),
                 elevation: 10,
               ),
@@ -57,7 +98,12 @@ class myHomePage extends StatelessWidget {
                 ),
                 child: Container(
                     padding: EdgeInsets.only(left: 20.0,top: 20.0),
-                    child: Text("Today Positive Cases: -")
+                    child: Column(
+                      children: [
+                        Text("Today Positive Cases: -"),
+                        Text('${newCases}'),
+                      ],
+                    )
                 ),
                 elevation: 10,
               ),
@@ -73,7 +119,12 @@ class myHomePage extends StatelessWidget {
                 ),
                 child: Container(
                     padding: EdgeInsets.only(left: 20.0,top: 20.0),
-                    child: Text("Today Death Cases: -")
+                    child: Column(
+                      children: [
+                        Text("Today Death Cases: -"),
+                        Text('${todayDeathCases}'),
+                      ],
+                    )
                 ),
                 elevation: 10,
               ),
@@ -89,15 +140,21 @@ class myHomePage extends StatelessWidget {
                 ),
                 child: Container(
                     padding: EdgeInsets.only(left: 20.0,top: 20.0),
-                    child: Text("Total Death cases: -")
+                    child: Column(
+                      children: [
+                        Text("Total Death cases: -"),
+                        Text('${totaldeath}'),
+                      ],
+                    )
                 ),
                 elevation: 10,
               ),
             ),
-            Text('Covid-19 News update'),
+            Text('R.V.P.S.Akesh',style: TextStyle(fontSize: 20.0),)
           ],
         ),
       ),
     );
   }
 }
+
